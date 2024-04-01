cf7a4d2b3912001ad450625f79add5dae7c82512
lijc210 <lijc210@163.com>
2024-04-01 18:32:23 +0800

    up

--- sw.js
+++ sw.js
@@ -1,41 +1,57 @@
-self.addEventListener("install", async (event) => {
-  console.log("sw install");
-});
-
-self.addEventListener("fetch", async (event) => {
-  console.log("sw fetch");
-});
-
+// 定义静态资源列表
 const staticAssets = ["./", "./app.css", "./app.js"];
 
+// 当 Service Worker 安装时触发，用于缓存静态资源
 self.addEventListener("install", async (event) => {
+  console.log("sw install"); // 在控制台输出安装信息
+
+  // 打开一个名为 "static-assets" 的缓存
   const cache = await caches.open("static-assets");
+
+  // 将静态资源添加到缓存中
   cache.addAll(staticAssets);
 });
 
+// 当页面发起网络请求时触发，用于拦截网络请求并处理缓存
 self.addEventListener("fetch", async (event) => {
+  console.log("sw fetch"); // 在控制台输出网络请求信息
+
+  // 获取请求对象
   const req = event.request;
+  // 解析请求的 URL
   const url = new URL(req.url);
 
+  // 如果请求的是同源资源，则使用缓存优先策略
   if (url.origin === location.origin) {
-    event.respondWith(cacheFirst(req));
+    event.respondWith(cacheFirst(req)); // 使用 cacheFirst 函数处理请求
   } else {
-    event.respondWith(networkFirst(req));
+    // 否则使用网络优先策略
+    event.respondWith(networkFirst(req)); // 使用 networkFirst 函数处理请求
   }
 });
 
+// 定义缓存优先策略：先检查缓存是否有匹配的资源，若有则直接返回缓存中的响应，否则向网络发起请求获取响应
 async function cacheFirst(req) {
+  // 尝试从缓存中获取匹配请求的响应
   const cachedResponse = await caches.match(req);
+  // 如果缓存中存在匹配的响应，则直接返回缓存中的响应，否则向网络发起请求获取响应
   return cachedResponse || fetch(req);
 }
 
+// 定义网络优先策略：先向网络发起请求获取响应，若请求失败则从缓存中获取响应，若缓存中没有则返回错误信息
 async function networkFirst(req) {
+  // 打开一个名为 "topics-dynamic" 的缓存
   const cache = await caches.open("topics-dynamic");
+
   try {
+    // 向网络发起请求获取响应
     const res = await fetch(req);
+    // 将获取的响应存入缓存中
     cache.put(req, res.clone());
+    // 返回获取的响应
     return res;
   } catch (error) {
+    // 如果请求失败，则从缓存中获取匹配请求的响应
     return await cache.match(req);
   }
 }
